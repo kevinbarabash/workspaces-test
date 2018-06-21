2374c34b9cb9120ac88875e495fa17c06d96b2cc
Kevin Barabash <kevinb@khanacademy.org>
2018-06-20 17:23:25 -0700

    added more exclamations and bumped version to -1

--- packages/foo/index.js
+++ packages/foo/index.js
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export default class Foo extends React.Component {
     render() {
-        return <h1>FOO!</h1>;
+        return <h1>FOO!!!</h1>;
     }
 }
 Foo.foo = "foo";
